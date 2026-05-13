d89200c644e9f2d0dc7f895c90b35b7eb0df51aa
Da1ki11 <148402110+Da1ki11@users.noreply.github.com>
2026-05-13 16:08:21 +0800

    Refactor exportImageFromGEE.py for clarity and updates

--- exportImageFromGEE.py
+++ exportImageFromGEE.py
@@ -13,12 +13,9 @@ from rasterio.warp import transform_bounds
 import utils.gee_utils as gee_utils
 
 ee.Authenticate()
-ee.Initialize(project = 'ee-wangyanrs') #ee-jiashuang0207/ ee-wangyanrs / muqiurs
+ee.Initialize()
 
-# region =======变量声明========================================================================
-# 0.25°的allGrids共包含16186个  1°的共包含1121个
 allGrids_0d25_CHN = ee.FeatureCollection("projects/ee-wangyanrs/assets/EBIA_CSM/CHN_grids_1d")
-
 export_folder ='CHN_percentiles_'
 grid_count = 1121
 strat_grid = 0 # [0,112,224,336,448,560,672,784,896,1008] 
@@ -30,14 +27,8 @@ cloud_pct = 20
 if strat_grid == 1008:
     exporting_grids_num = 113
 target_years = np.full(exporting_grids_num,1992) 
-# or generate random year
-# candidates = np.arange(1990, 2021, 5)  # 1990, 1995, ..., 2020
-# target_years = np.random.choice(candidates, size = grid_count, replace=True)
-
-# endregion
 
 
-# region =======函数定义========================================================================
 def imgComposite_by_geom(roi,buffer_meter=1000):
     if buffer_meter > 0:
         roi = roi.geometry().buffer(buffer_meter).bounds()
@@ -72,11 +63,6 @@ def collect_feature_image(year, roi, buffer_meter=1000):
     else:
         roi = roi.geometry()
     
-    # year = ee.String(grid_name).split('_').get(-1)
-    # year = ee.Number.parse(year)
-    # start_date = ee.Algorithms.If(year.lte(2000), ee.Date.fromYMD(year.subtract(2),1,1), ee.Date.fromYMD(year.subtract(1),7,1))
-    # end_date = ee.Algorithms.If(year.lte(2000), ee.Date.fromYMD(year.add(3),1,1), ee.Date.fromYMD(year.add(1),7,1))
-    # log_info = ee.String('collecting images for ').cat(ee.String(grid_name))
     if year < 2000:
         # start_date = str(year-2) + '-01-01'
         # end_date = str(year+3) + '-01-01'
@@ -132,8 +118,6 @@ def collect_seasonal_feature(year, roi,buffer_meter=1000):
     merged_img = ee.Image.cat(merged_img).addBands(gee_utils.get_terrain(roi)).toInt16()
     return merged_img
 
-# 每个制图年都收集前后五年的单传感器图像集合并做分值合成
-# 若year是1990到2010任意一年，则定义sensor= 'LT05',若year是2015到2020任意一年，则定义sensor='LC08'
 def collect_feature_5year1sensor(year,roi,buffer_meter=1000):
     grid_name = roi.get('filename')
     if buffer_meter > 0:
@@ -205,11 +189,8 @@ def createFeatureCollectionFromGeojson(geojson_file, start_grid_index, grid_coun
     fc = ee.FeatureCollection(features[start_grid_index : start_grid_index + grid_count]) #features[0:2]
     return fc
 
-# endregion
-
 # region =======main function==================================================================
 if __name__ == "__main__":
-    #====================如果是收集中国地区1d tile内的特征图像========================#
     if strat_grid == 1008:
         exporting_grids_num = 113
     allGrids_0d25_CHN_list = allGrids_0d25_CHN.toList(1121)
@@ -245,69 +226,4 @@ if __name__ == "__main__":
         )
         task.start()
         print(f'{log_info} for tile {grid_name}')
-
-
-    #====================如果是收集训练样本位置的特征图像========================#
-    # ----------------可以根据矢量文件的每个polygon作为roi-----------------------
-    # fc_file = r'G:\EBIA-CSM_DATA\builtup_from_products\3-builtup_mask\1000grids\label_grids.geojson'
-    # exporting_grids = createFeatureCollectionFromGeojson(fc_file,strat_grid,exporting_grids_num,target_years).toList(exporting_grids_num)
-    # arr = list(range(1028))    # 0到1028的列表
-    # random.shuffle(arr)        # 原地随机打乱
-    # for i in arr[0:129]:# range(0, exporting_grids_num, 1):
-    #     exporting_grid = ee.Feature(exporting_grids.get(i))
-    #     target_year = target_years[i]
-    #     img = collect_feature_image(target_year, exporting_grid, buffered_meter)
-    #     filename = exporting_grid.get('filename').getInfo()
-    #     log_info = img.get('logInfo').getInfo()
-    #     img_geometry = img.geometry()
-    #     task = ee.batch.Export.image.toDrive(
-    #         image =img,
-    #         description = filename,#'featureImage_'+str(targete_year)+'_'+"{:05d}".format(i+1+strat_grid),
-    #         folder = export_folder+str(target_year),
-    #         fileNamePrefix=filename,#'featureImage_'+str(targete_year)+'_'+"{:05d}".format(i+1+strat_grid),
-    #         region =img_geometry,
-    #         scale =30,
-    #         maxPixels =1e13
-    #     )
-    #     task.start()
-    #     print(f'{log_info} for tile {filename}')
-
-    # ----------------还可以参考已经有标签tif文件作为roi-----------------------
-    # ref_folder = r'G:\EBIA-CSM_DATA\Dataset_for_Unet\CSM_dataset\1st_addTiles_backgroundClass\labelImage'
-    # ref_files = sorted([f for f in os.listdir(ref_folder) if f.lower().endswith('.tif')])
-    # for i in range(0,len(ref_files)): #len(ref_files)
-    #     ref_file = ref_files[i]
-    #     ref_path = os.path.join(ref_folder, ref_file)
-    #     try:
-    #         if target_years is not None:
-    #             year = target_years[i]
-    #         else:
-    #             year = int(os.path.splitext(ref_file)[0].split('_')[-1])
-    #         # years = np.random.choice(candidates, size = 2, replace=True)
-    #         with rasterio.open(ref_path) as src:
-    #             bounds = src.bounds
-    #             bounds = transform_bounds(src.crs, 'EPSG:4326', *bounds)
-    #             # 将rasterio的bounds转换为GEE的ee.Geometry.Rectangle
-    #             roi = ee.Geometry.Rectangle([bounds[0], bounds[1], bounds[2], bounds[3]])
-    #             roi = ee.Feature(roi, {"filename":ref_file})
-    #             descriptions = ref_file.split('_')
-    #             description = descriptions[0] + '_' + descriptions[1] + '_' + descriptions[2] + '_' + str(year) + '.tif'
-    #             img = collect_feature_image(year, roi, buffered_meter)
-    #             task = ee.batch.Export.image.toDrive(
-    #                 image =img,
-    #                 description = description,#'featureImage_'+str(targete_year)+'_'+"{:05d}".format(i+1+strat_grid),
-    #                 folder = export_folder+str(year),
-    #                 fileNamePrefix=description,#'featureImage_'+str(targete_year)+'_'+"{:05d}".format(i+1+strat_grid),
-    #                 region =roi.geometry(),
-    #                 scale =30,
-    #                 maxPixels =1e13
-    #             )
-    #             task.start()
-    #             print(f'exporting multi sensor composited image for training grid {ref_file} in {year}')
-    #     except Exception:
-    #         print(Exception)
-    #         continue 
-
-
-
 # endregion
